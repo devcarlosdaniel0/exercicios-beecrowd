6c94c677a8febaf8b1ca745c13a50c99a973015c
Carlos <cadanielmarchesin@gmail.com>
2024-07-18 11:52:04 -0300

    Media Ponderada

---
+++ beecrowd-java/src/com/beecrowd/exercicios/iniciante/test/Media1.java
@@ -0,0 +1,14 @@
+package com.beecrowd.exercicios.iniciante.test;
+
+import java.util.Scanner;
+
+public class Media1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+        double MEDIA = (A * 3.5 + B * 7.5) / 11;
+        System.out.printf("MEDIA = %.5f%n", MEDIA);
+        scanner.close();
+    }
+}
