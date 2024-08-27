a10998266fd3d29d5a1a2bed650cb5b77d88147c
Carlos <cadanielmarchesin@gmail.com>
2024-08-27 14:55:39 -0300

    Update BinarySearch.java

--- beecrowd-java/src/com/beecrowd/exercicios/iniciante/test/BinarySearch.java
+++ beecrowd-java/src/com/beecrowd/exercicios/iniciante/test/BinarySearch.java
@@ -5,17 +5,22 @@ import java.util.List;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(List.of(5,3,2,5,6,7,8,9,9,11));
+        List<Integer> list = new ArrayList<>();
+        addListLength(list, 1024);
         list.sort(Integer::compareTo);
         System.out.println(list);
 
-        int index = binarySearch(list, 11);
+        int index = binarySearch(list, 1024);
         System.out.println(index);
     }
 
     public static int binarySearch(List<Integer> list, int num) {
         int baixo = 0;
         int alto = list.size() - 1;
+
+        if (list.isEmpty() || num < list.get(0) || num > list.get(list.size() - 1)) {
+            return -1;
+        }
 
         while (baixo <= alto) {
             int meio = (baixo + alto) / 2;
@@ -28,7 +33,14 @@ public class BinarySearch {
                 baixo = meio + 1;
             }
         }
-
+        
         return -1;
+    }
+
+    public static List<Integer> addListLength(List<Integer> list, int tamanho) {
+        for (int i = 0; i <= tamanho; i++) {
+            list.add(i);
+        }
+        return list;
     }
 }
