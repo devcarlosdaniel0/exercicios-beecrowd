a1e3d07fd4782efc0cd38286188448ce8aae354d
Carlos <cadanielmarchesin@gmail.com>
2024-09-12 17:39:07 -0300

    Create TesteDeSelecao.java

---
+++ beecrowd-java/src/com/beecrowd/exercicios/iniciante/test/TesteDeSelecao.java
@@ -0,0 +1,21 @@
+package com.beecrowd.exercicios.iniciante.test;
+
+import java.util.Scanner;
+
+public class TesteDeSelecao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        if (b > c && d > a && (c + d) > (a + b) && c > 0 && d > 0 && a % 2 == 0) {
+            System.out.println("Valores aceitos");
+        } else {
+            System.out.println("Valores nao aceitos");
+        }
+
+        scanner.close();
+    }
+}
